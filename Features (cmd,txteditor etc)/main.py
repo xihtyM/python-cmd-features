6f6312b785e27ce409c844560142bba78943a896
BuildTools <unconfigured@null.spigotmc.org>
2022-06-16 20:45:43 +0100

    lmao

--- Features (cmd,txteditor etc)/main.py
+++ Features (cmd,txteditor etc)/main.py
@@ -1,4 +1,4 @@
-import os, texteditor, dec, find, gcwd_, runpy, colorama, clrs, pyplus, math;
+import os, texteditor, dec, find, gcwd_, runpy, colorama, clrs, pyplus, math, subprocess;
 from py_console import console;
 
 # Startup
@@ -6,6 +6,22 @@ from py_console import console;
 os.system("");
 
 # Start functions
+
+def zeroBytes(path,_size,delete):
+    if(os.path.isfile(path)): console.error("Error:",path,"is a file, not a directory."); return -1;
+    for x,v in enumerate(os.listdir(path)):
+        truePath = path+"/"+v;
+        if(os.path.isfile(truePath)):
+            size = os.path.getsize(truePath);
+            if(size == _size and delete == False):
+                print(str(_size),"bytes:\t",truePath);
+                continue;
+            if(size == _size and delete == True):
+                os.remove(truePath);
+                print("Removed file:\t",truePath);
+            continue;
+        zeroBytes(truePath,_size,delete);
+    return 0;
 
 def convertBytes(bytes):
     if(bytes == 0): return "0 byte(s)\t\t";
@@ -34,7 +50,7 @@ def getcwd():
 def updateDir():
     try:
         with open(getcwd()+"/vars/dir","r") as d:
-            return d.read();
+            return d.read().replace("â €","⠀");
     except Exception as err:
         console.error("Error:",err);
 
@@ -43,10 +59,21 @@ def updatePath():
         pathVars = {};
         for i,v in enumerate(os.listdir(getcwd()+"/vars/path/")):
             with open(getcwd()+"/vars/path/"+v) as path:
-                pathVars[v] = path.read();
+                pathVars[v] = path.read().replace("â €","⠀");
     except Exception as err:
         console.error("Error:",err);
     return pathVars;
+
+def openPyEdit(path_to_file):
+    try:
+        if(not os.path.exists(path_to_file)): console.error("Error: Path doesn't exist."); return 1;
+        if(not os.path.exists(vars.path["idle"])): console.error("Error: IDLE Path doesn't exist."); return 1;
+        if(not os.path.isfile(path_to_file)): console.error("Error: Path must be a file."); return 1;
+        subprocess.call([vars.path["idle"], path_to_file], shell=True);
+        return 0;
+    except Exception as err:
+        console.error("Error:",err);
+        return 1;
 
 # Start variables
 
@@ -58,13 +85,71 @@ class vars:
 
 def cmd(command):
 
+    # Find command
+    
+    if(command[0:4] == "find"):
+        try:
+            fileSize = command.find("-s ");
+            if(fileSize != -1):
+                fileSize = int(command[fileSize+3:].rstrip());
+                for x,v in enumerate(os.listdir(vars.directory)):
+                    truePath = vars.directory+"/"+v;
+                    if(os.path.isfile(truePath)):
+                        if(os.path.getsize(truePath) == fileSize):
+                            print(str(fileSize),"bytes:\t",truePath);
+                        continue;
+                    zeroBytes(truePath,fileSize,False);
+                return 0;
+            maxSize = command.find("+m ");
+            minSize = command.find("-m ");
+            if(maxSize != -1 and minSize != -1):
+                if(maxSize > minSize):
+                    minSize = int(command[minSize+3:command.find(" ",minSize+3)].rstrip());
+                    maxSize = int(command[maxSize+3:].rstrip());
+                else:
+                    minSize = int(command[minSize+3:].rstrip());
+                    maxSize = int(command[maxSize+3:command.find(" ",maxSize+3)].rstrip());
+                for x,v in enumerate(os.listdir(vars.directory)):
+                    truePath = vars.directory+"/"+v;
+                    if(os.path.isfile(truePath)):
+                        if(os.path.getsize(truePath) >= minSize and os.path.getsize(truePath) <= maxSize):
+                            print(str(os.path.getsize(truePath)),"bytes:\t",truePath);
+                        continue;
+                    #zeroBytes(truePath,fileSize,False);
+                return 0;
+            console.error("Error: Incorrect usage.");
+            return 1;
+        except Exception as err:
+            console.error("Error:",err);
+            return 1;
+
+    # Shutdown command
+
+    if(command[0:8] == "shutdown"):
+        try:
+            operator = command[9:11].lstrip().rstrip();
+            # Shutdown
+            if(operator == "-s"): os.system("shutdown -s 50"); os.system("shutdown /a");
+            # Restart
+            if(operator == "-r"): os.system("shutdown -r 0"); os.system("shutdown /a");
+            # Log off
+            if(operator == "-l"): os.system("shutdown -l 0"); os.system("shutdown /a");
+            return 0;
+        except Exception as err:
+            console.error("Error:",err);
+            return 1;
+
+    # Find files command
+
+
+
     # Run from path
 
     if(command[0:4] == "runp"):
         try:
             if(not os.path.exists(getcwd()+"/vars/path/"+command[5:].lstrip())): console.error("Error: Path variable does not exist."); return 1;
             path = vars.path[command[5:].lstrip()];
-            if(not os.path.exists(path)): console.error("Error: Path variable is invalid (It doesn't exist)."); return 1;
+            if(not os.path.exists(path)): console.error("Error: Path variable is invalid (It doesn't exist).",path); return 1;
             os.startfile(path);
             return 0;
         except Exception as err:
@@ -76,8 +161,17 @@ def cmd(command):
     if(command[0:4] == "path"):
         try:
             firstSpace = command.find(" ");
+            if(firstSpace == -1): console.error("Error: Incorrect usage. See -py for help."); return 1;
             operator = command[firstSpace:firstSpace+3].lstrip().rstrip();
             path_var = command[firstSpace+3:].lstrip().rstrip();
+            if(operator == "-c"):
+                with open(getcwd()+"/vars/path/"+path_var,"x") as createPath:
+                    file = input("Replace with (" + vars.directory + ") >> ");
+                    fullFile = vars.directory+"/"+file;
+                    if(not os.path.exists(fullFile)): console.error("Error: Path does not exist ('"+fullFile+"')"); return 1;
+                    if(not os.path.isfile(fullFile)): console.error("Error: Path must be a file."); return 1;
+                    createPath.write(fullFile.replace("⠀","â €"));
+                return 0;
             if(not os.path.exists(getcwd()+"/vars/path/"+path_var)): console.error("Error: Path does not exist ('"+path_var+"')"); return 1;
             if(operator == "-e"):
                 file = input("Replace with (" + vars.directory + ") >> ");
@@ -85,13 +179,14 @@ def cmd(command):
                 if(not os.path.exists(fullFile)): console.error("Error: Path does not exist ('"+fullFile+"')"); return 1;
                 if(not os.path.isfile(fullFile)): console.error("Error: Path must be a file."); return 1;
                 with open(getcwd()+"/vars/path/"+path_var,"w") as writePath:
-                    writePath.write(fullFile);
+                    writePath.write(fullFile.replace("⠀","â €"));
                 vars.path = updatePath();
                 return 0;
             if(operator == "-r"):
                 with open(getcwd()+"/vars/path/"+path_var,"r") as readPath:
-                    print(path_var+":",readPath.read());
+                    print(path_var+":",readPath.read().replace("â €","⠀"));
                 return 0;
+
         except Exception as err:
             console.error("Error:",err);
             return 1;
@@ -103,7 +198,7 @@ def cmd(command):
             if(vars.directory.count("/") == 0): console.error("Error: Cannot have a null path."); return 1;
             vars.directory = vars.directory[0:find.find(vars.directory,"/",vars.directory.count("/")-1)-1];
             with open(getcwd()+"/vars/dir","w") as d:
-                d.write(vars.directory);
+                d.write(vars.directory.replace("⠀","â €"));
             vars.directory = updateDir();
             return 0;
         except Exception as err:
@@ -249,12 +344,12 @@ def cmd(command):
 
             if(os.path.exists(path)):
                 with open(getcwd()+"/vars/dir","w") as d:
-                    d.write(path);
+                    d.write(path.replace("⠀","â €"));
                 vars.directory = updateDir();
                 return 0;
             if(os.path.exists(vars.directory+"/"+path)):
                 with open(getcwd()+"/vars/dir","w") as d:
-                    d.write(vars.directory+"/"+path);
+                    d.write(vars.directory.replace("⠀","â €")+"/"+path.replace("⠀","â €"));
                 vars.directory = updateDir();
                 return 0;
             
